53086b1f5b1db4d06d23c0fdd4adce6f5ad240b5
J-P Nurmi <jp.nurmi@canonical.com>
2022-04-28 05:44:49 +0200

    Replace the error enum with exception classes

--- lib/src/fwupd_client.dart
+++ lib/src/fwupd_client.dart
@@ -118,67 +118,98 @@ enum FwupdInstallFlag {
   ignorePower,
 }
 
-enum FwupdError {
-  /// Internal error
-  internal,
-
-  /// Installed newer firmware version
-  versionNewer,
-
-  /// Installed same firmware version
-  versionSame,
-
-  /// Already set be be installed offline
-  alreadyPending,
-
-  /// Failed to get authentication
-  authFailed,
-
-  /// Failed to read from device
-  read,
-
-  /// Failed to write to the device
-  write,
-
-  /// Invalid file format
-  invalidFile,
-
-  /// No matching device exists
-  notFound,
-
-  /// Nothing to do
-  nothingToDo,
-
-  /// Action was not possible
-  notSupported,
-
-  /// Signature was invalid
-  signatureInvalid,
-
-  /// AC power was required
-  acPowerRequired,
-
-  /// Permission was denied
-  permissionDenied,
-
-  /// User has configured their system in a broken way
-  brokenSystem,
-
-  /// The system battery level is too low
-  batteryLevelTooLow,
-
-  /// User needs to do an action to complete the update
-  needsUserAction,
-}
-
 class FwupdException implements Exception {
-  final FwupdError error;
   final dynamic message;
 
-  const FwupdException(this.error, [this.message]);
+  const FwupdException([this.message]);
 
   @override
-  String toString() => 'FwupdException(${error.name}: $message)';
+  String toString() => '$runtimeType: $message';
+}
+
+/// Internal error
+class FwupdInternalException extends FwupdException {
+  const FwupdInternalException([dynamic message]) : super(message);
+}
+
+/// Installed newer firmware version
+class FwupdVersionNewerException extends FwupdException {
+  const FwupdVersionNewerException([dynamic message]) : super(message);
+}
+
+/// Installed same firmware version
+class FwupdVersionSameException extends FwupdException {
+  const FwupdVersionSameException([dynamic message]) : super(message);
+}
+
+/// Already set be be installed offline
+class FwupdAlreadyPendingException extends FwupdException {
+  const FwupdAlreadyPendingException([dynamic message]) : super(message);
+}
+
+/// Failed to get authentication
+class FwupdAuthFailedException extends FwupdException {
+  const FwupdAuthFailedException([dynamic message]) : super(message);
+}
+
+/// Failed to read from device
+class FwupdReadException extends FwupdException {
+  const FwupdReadException([dynamic message]) : super(message);
+}
+
+/// Failed to write to the device
+class FwupdWriteException extends FwupdException {
+  const FwupdWriteException([dynamic message]) : super(message);
+}
+
+/// Invalid file format
+class FwupdInvalidFileException extends FwupdException {
+  const FwupdInvalidFileException([dynamic message]) : super(message);
+}
+
+/// No matching device exists
+class FwupdNotFoundException extends FwupdException {
+  const FwupdNotFoundException([dynamic message]) : super(message);
+}
+
+/// Nothing to do
+class FwupdNothingToDoException extends FwupdException {
+  const FwupdNothingToDoException([dynamic message]) : super(message);
+}
+
+/// Action was not possible
+class FwupdNotSupportedException extends FwupdException {
+  const FwupdNotSupportedException([dynamic message]) : super(message);
+}
+
+/// Signature was invalid
+class FwupdSignatureInvalidException extends FwupdException {
+  const FwupdSignatureInvalidException([dynamic message]) : super(message);
+}
+
+/// AC power was required
+class FwupdAcPowerRequiredException extends FwupdException {
+  const FwupdAcPowerRequiredException([dynamic message]) : super(message);
+}
+
+/// Permission was denied
+class FwupdPermissionDeniedException extends FwupdException {
+  const FwupdPermissionDeniedException([dynamic message]) : super(message);
+}
+
+/// User has configured their system in a broken way
+class FwupdBrokenSystemException extends FwupdException {
+  const FwupdBrokenSystemException([dynamic message]) : super(message);
+}
+
+/// The system battery level is too low
+class FwupdBatteryLevelTooLowException extends FwupdException {
+  const FwupdBatteryLevelTooLowException([dynamic message]) : super(message);
+}
+
+/// User needs to do an action to complete the update
+class FwupdNeedsUserActionException extends FwupdException {
+  const FwupdNeedsUserActionException([dynamic message]) : super(message);
 }
 
 @immutable
@@ -915,39 +946,39 @@ class FwupdClient {
       var errorMessage = errorResponse.values.firstOrNull?.toNative();
       switch (errorResponse.errorName) {
         case 'org.freedesktop.fwupd.Internal':
-          throw FwupdException(FwupdError.internal, errorMessage);
+          throw FwupdInternalException(errorMessage);
         case 'org.freedesktop.fwupd.VersionNewer':
-          throw FwupdException(FwupdError.versionNewer, errorMessage);
+          throw FwupdVersionNewerException(errorMessage);
         case 'org.freedesktop.fwupd.VersionSame':
-          throw FwupdException(FwupdError.versionSame, errorMessage);
+          throw FwupdVersionSameException(errorMessage);
         case 'org.freedesktop.fwupd.AlreadyPending':
-          throw FwupdException(FwupdError.alreadyPending, errorMessage);
+          throw FwupdAlreadyPendingException(errorMessage);
         case 'org.freedesktop.fwupd.AuthFailed':
-          throw FwupdException(FwupdError.authFailed, errorMessage);
+          throw FwupdAuthFailedException(errorMessage);
         case 'org.freedesktop.fwupd.Read':
-          throw FwupdException(FwupdError.read, errorMessage);
+          throw FwupdReadException(errorMessage);
         case 'org.freedesktop.fwupd.Write':
-          throw FwupdException(FwupdError.write, errorMessage);
+          throw FwupdWriteException(errorMessage);
         case 'org.freedesktop.fwupd.InvalidFile':
-          throw FwupdException(FwupdError.invalidFile, errorMessage);
+          throw FwupdInvalidFileException(errorMessage);
         case 'org.freedesktop.fwupd.NotFound':
-          throw FwupdException(FwupdError.notFound, errorMessage);
+          throw FwupdNotFoundException(errorMessage);
         case 'org.freedesktop.fwupd.NothingToDo':
-          throw FwupdException(FwupdError.nothingToDo, errorMessage);
+          throw FwupdNothingToDoException(errorMessage);
         case 'org.freedesktop.fwupd.NotSupported':
-          throw FwupdException(FwupdError.notSupported, errorMessage);
+          throw FwupdNotSupportedException(errorMessage);
         case 'org.freedesktop.fwupd.SignatureInvalid':
-          throw FwupdException(FwupdError.signatureInvalid, errorMessage);
+          throw FwupdSignatureInvalidException(errorMessage);
         case 'org.freedesktop.fwupd.AcPowerRequired':
-          throw FwupdException(FwupdError.acPowerRequired, errorMessage);
+          throw FwupdAcPowerRequiredException(errorMessage);
         case 'org.freedesktop.fwupd.PermissionDenied':
-          throw FwupdException(FwupdError.permissionDenied, errorMessage);
+          throw FwupdPermissionDeniedException(errorMessage);
         case 'org.freedesktop.fwupd.BrokenSystem':
-          throw FwupdException(FwupdError.brokenSystem, errorMessage);
+          throw FwupdBrokenSystemException(errorMessage);
         case 'org.freedesktop.fwupd.BatteryLevelTooLow':
-          throw FwupdException(FwupdError.batteryLevelTooLow, errorMessage);
+          throw FwupdBatteryLevelTooLowException(errorMessage);
         case 'org.freedesktop.fwupd.NeedsUserAction':
-          throw FwupdException(FwupdError.needsUserAction, errorMessage);
+          throw FwupdNeedsUserActionException(errorMessage);
         default:
           rethrow;
       }
