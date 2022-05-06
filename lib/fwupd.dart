a0664d77aff28116c8b3ce8b3bb751687dc5918c
J-P Nurmi <jp.nurmi@canonical.com>
2022-05-06 08:24:13 +0200

    Split to smaller files

--- lib/fwupd.dart
+++ lib/fwupd.dart
@@ -1,1 +1,6 @@
 export 'src/fwupd_client.dart';
+export 'src/fwupd_device.dart';
+export 'src/fwupd_exceptions.dart';
+export 'src/fwupd_plugin.dart';
+export 'src/fwupd_release.dart';
+export 'src/fwupd_remote.dart';
---
+++ lib/src/fwupd_plugin.dart
@@ -0,0 +1,10 @@
+class FwupdPlugin {
+  final String name;
+
+  FwupdPlugin({
+    required this.name,
+  });
+
+  @override
+  String toString() => 'FwupdDevice(name: $name)';
+}
